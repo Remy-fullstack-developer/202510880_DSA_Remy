58c33d7c95e75b15d604417fa7464ae257627d6e
Remy-fullstack-developer <nsabimanajeanremy39@gmail.com>
2026-05-13 18:14:47 +0200

    Add files via upload

---
+++ swapping.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	
+	int a=8;
+	int b=10;
+	cout<<"before swapping"<<endl;
+		cout<<"  a  :"<<a<<  "  b:  "<<b<<endl;
+	int temp=a;
+	a=b;
+	b=temp;
+	
+		cout<<"after swapping"<<endl;
+	cout<<"  a  :"<<a<<  "  b:  "<<b<<endl;
+	
+	
+	
+	return 0;
+}
